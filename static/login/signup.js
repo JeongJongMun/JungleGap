d8da0f5969936fb0f7ccee9e439ff7f692ce4dd9
정종문 <whdans4005@naver.com>
2024-01-11 01:41:39 +0900

    11일 마지막 합치기

--- static/login/signup.js
+++ static/login/signup.js
@@ -1,22 +1,4 @@
-tailwind.config = {
-    theme: {
-        extend: {
-            fontFamily: {
-                jgF1: ['Pretendard-Regular', 'WarhavenB']
-            },
-            colors: {
-                customJgColor1: "#254E3CFF",
-                customJgColor2: "#FF9800FF",
-                customJgColor3: "#5F8670CC"
-            },
-            fontSize: {
-                customFontSize1: '100px',
-            },
-        },
-    },
-};
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     function signup() {
         $.ajax({
             type: "POST",
